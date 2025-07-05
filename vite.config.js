a6b97f2b3753d8b21f2bf485d54220f776e51201
hunterdev90 <Hunterdev90@outlook.com>
2025-07-05 02:36:23 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
     plugins: [
@@ -10,4 +12,12 @@ export default defineConfig({
         }),
         react(),
     ],
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss(),
+                autoprefixer(),
+            ],
+        },
+    },
 });
